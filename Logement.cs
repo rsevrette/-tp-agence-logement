47e07833fb9fc544b25d9ec8b9943271123022f0
rsevrette <romain.sevrette@edouardgand.fr>
2026-03-24 11:48:39 +0100

    ajout Logement

---
+++ Logement.cs
@@ -0,0 +1,27 @@
+class Logement
+{
+    protected string reference;
+    protected string adresse;
+    protected int surface;
+    protected double loyerBase;
+    protected bool disponible;
+
+    public Logement(string reference, string adresse, int surface, double loyerBase, bool disponible)
+    {
+        if (surface < 0) Console.WriteLine("la surface doit etre positive");
+        else this.surface = surface;
+        if (loyerBase < 0) Console.WriteLine("le loyer doit etre positif");
+        else this.loyerBase = loyerBase;
+        this.reference = reference;
+        this.adresse = adresse;
+        this.disponible = disponible;
+    }
+    public void Afficher()
+    {
+        Console.WriteLine($"{reference} | {adresse} | surface: {surface} | loyer: {loyerBase} | disponible: {disponible}");
+    }
+    public void CalculerLoyer()
+    {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Partie 1
+        Logement logement1 = new Logement("maison1", "rue", 100, 500, true);
+        logement1.Afficher();
+        Logement logement2 = new Logement("appart1", "rue", -100, -500, false);
+        logement2.Afficher();
     }
 }
